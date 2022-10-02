feacb62480c0f39755739f3c62b1d5bdb1ee6490
kevin <kevin.mazure.km@gmail.com>
2022-10-02 15:52:06 +0200

    listView getAllPosts

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -16,46 +16,80 @@ class HomePage extends StatefulWidget {
 }
 
   class _HomePageState extends State<HomePage>{
-  List<Post>? posts;
-  List<User>? users;
-  var isLoaded = false;
+    List<Post>? posts;
+    var isLoaded = false;
 
+    @override
+    void initState() {
+      super.initState();
 
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
+      //fetch data from API
+      getData();
+    }
 
+    getData() async {
+      posts = await ServicePost().getPosts();
+      if (posts != null) {
+        setState(() {
+          isLoaded = true;
+        });
+      }
+    }
 
-
-  getData() async{
-    posts = await ServicePost().getPosts();
-    if(posts!=null){
-      setState(() {
-        isLoaded = true;
-      });
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Posts'),
+        ),
+        body: Visibility(
+          visible: isLoaded,
+          replacement: const Center(
+            child: CircularProgressIndicator(),
+          ),
+          child: ListView.builder(
+            itemCount: posts?.length,
+            itemBuilder: (context, index) {
+              return Container(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            posts![index].name,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            posts![index].caption ?? '',
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      );
     }
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Visibility(
-        visible: isLoaded,
-        child: ListView.builder( itemCount:posts?.length ,itemBuilder: (context, index){
-
-          return Container(
-            child: Text(posts![index].caption),
-          );
-        }),
-        replacement: const Center(
-          child: CircularProgressIndicator(),
-        ),
-      ),
-    );
-  }
-}
